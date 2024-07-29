0fd00ea4ce81daccd16ef16705124333ce233765
OmarKnows <omarknows98@gmail.com>
2024-07-29 16:01:26 +0300

    fixed folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RootLayoutComponent } from './layouts/root-layout/root-layout.component';
+import { UsersComponent } from './modules/users/users.component';
+import { EmployeesComponent } from './modules/employees/employees.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: '',
+    component: RootLayoutComponent,
+    children: [
+      { path: 'employees', component: EmployeesComponent },
+      { path: 'users ', component: UsersComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
